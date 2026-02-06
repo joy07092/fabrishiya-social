b5d9f5cc10be6a83cec361a3b9ded4442e0dfa56
joy07092 <126982931+joy07092@users.noreply.github.com>
2026-02-06 22:39:35 +0600

    Add files via upload

--- qr.py
+++ qr.py
@@ -1,16 +1,16 @@
 import qrcode
 
-url = "https://yourusername.github.io/social-qr/"
+URL = "https://joy07092.github.io/fabrishiya-social/"
 
 qr = qrcode.QRCode(
     version=None,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,  # important for print
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=12,
     border=4,
 )
 
-qr.add_data(url)
+qr.add_data(URL)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save("social_qr.png")
+img.save("fabrishiya_qr.png")
